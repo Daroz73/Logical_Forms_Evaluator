f47c459e56ef601b8c9d5c655cbf70860dfcf688
DayoRodriguez <rodriguezperezdayan@gmail.com>
2026-06-22 01:15:55 -0400

    Representation finished

--- core/evaluator.py
+++ core/evaluator.py
@@ -7,37 +7,32 @@ class Evaluator:
         self._cases:list[tuple[dict[str,bool], bool]] = []
         self._was_True = 0
         self._was_False = 0
-        self._evaluate(0, list(self.ast.get_variables()), [False for _ in self.ast.get_variables()])
+        self._evaluate(0, list(self.ast.get_variables()))
         self.is_tautology:bool = True if self._was_True == 2**len(self._evaluator.keys()) else False
         self.is_contradiction:bool = True if self._was_False == 2**len(self._evaluator.keys()) else False
         self.is_contingency: bool =  not(self.is_tautology or self.is_contradiction)
 
 
-    def _evaluate(self, index:int, vars:list[str], mask:list[bool]):
+    def _evaluate(self, index:int, vars:list[str]) -> None:
         if index == len(vars):
+            result = self.ast.evaluate(self._evaluator)
+            if result:
+                self._was_True += 1
+            else:
+                self._was_False += 1
+            self._cases.append((self._evaluator.copy(), result))
             return
-
-        if not mask[index]:
-            mask[index] = True
+        if vars[index] == "1":    
             self._evaluator[vars[index]] = True
-            if self.ast.evaluate(self._evaluator):
-                self._was_True += 1
-                self._cases.append((self._evaluator.copy(), True))
-            else:
-                self._was_False += 1
-                self._cases.append((self._evaluator.copy(), False))
-            
-            self._evaluate(index+1, vars, mask)
-
+            self._evaluate(index+1, vars)
+        elif vars[index] == "0":
             self._evaluator[vars[index]] = False
-            mask[index] = False        
-            if self.ast.evaluate(self._evaluator):
-                self._was_True += 1
-                self._cases.append((self._evaluator.copy(), True))
-            else:
-                self._was_False += 1
-                self._cases.append((self._evaluator.copy(), False))
-
+            self._evaluate(index+1, vars)
+        else:
+            self._evaluator[vars[index]] = True
+            self._evaluate(index+1, vars)
+            self._evaluator[vars[index]] = False
+            self._evaluate(index+1, vars)
 
     def veritative_table(self) -> str:
         table:str = ""
